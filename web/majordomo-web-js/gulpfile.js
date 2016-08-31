2362fb2c77d526cfb1b08f775ced509179a89b73
Dan Merritt <djosme@gmail.com>
2016-08-31 22:52:25 +0900

    Update gulpfile

--- web/majordomo-web-js/gulpfile.js
+++ web/majordomo-web-js/gulpfile.js
@@ -2,20 +2,20 @@ var gulp = require('gulp');
 var environments = require('gulp-environments');
 var tasks = require('./tools/gulp-tasks/');
 
+// Load application configuration.
+var appConfig = require('./config/appconfig.json');
+
 // Predefined environments from gulp-environments. Environent can be specified
-// for task invokation in a number of ways, including: gulp <task> --dev/--prod
+// for task invokation in a number of ways, including: gulp <task> --development
 // See gulp-environments documentation for additional patterns:
 // https://www.npmjs.com/package/gulp-environments 
 var development = environments.development;
 var production = environments.production;
 
 // Paths and files to remove on clean.
-var cleanPaths = [];
-
-// External libraries that don't require any processing. They will be injected into the 
-// index template--individually for dev and bundled and minified for prod. 
-var libScripts = [];
-var libStyles = [];
+var cleanPaths = [
+    appConfig.outputDir
+];
 
 gulp.task('clean', () => tasks.clean(cleanPaths));
 gulp.task('build', function(){});
